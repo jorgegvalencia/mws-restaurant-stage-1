a09a76ca7dd0c7fbe485e6db68cb9431e3782aab
jorgegvalencia <jorgegvalencia@gmail.com>
2018-05-15 18:18:08 +0200

    refactor: Clean logs in helper class

--- src/js/idbhelper.js
+++ src/js/idbhelper.js
@@ -17,8 +17,6 @@ class IDBHelper {
         var restaurantsStore = upgradeDb.createObjectStore('restaurants', {
           keyPath: 'id'
         });
-        // with 'keyPath' property we indicate which property of the object has to be treated as the unique key
-        // creates the index 'animal' over the object store 
         restaurantsStore.createIndex('id', 'id'); 
         restaurantsStore.createIndex('cuisine', 'cuisine_type');
         restaurantsStore.createIndex('neighborhood', 'neighborhood');
@@ -29,7 +27,7 @@ class IDBHelper {
   storeRestaurants(restaurants) {
     return this._dbPromise.then(function(db) {
       if (!db) return;
-      var tx = db.transaction('restaurants', 'readwrite'); // note here we have to specify the access type 'readwrite'
+      var tx = db.transaction('restaurants', 'readwrite');
       var restaurantsStore = tx.objectStore('restaurants');
       restaurants.forEach(restaurant => {
         restaurantsStore.put(restaurant);
@@ -57,7 +55,6 @@ class IDBHelper {
       return idIndex.get(+id);
     })
       .then((restaurant) => {
-        console.log('From local db:', restaurant);
         return restaurant || this.getStoredRestaurants()
           .then(function(restaurants) {
             return restaurants.find(restaurant => restaurant.id === id);
@@ -68,34 +65,3 @@ class IDBHelper {
 }
 
 module.exports = new IDBHelper();
-
-
-/*  
-  dbPromise.then(function(db) {
-    var tx = db.transaction('people');
-    var peopleStore = tx.objectStore('people');
-    var animalIndex = peopleStore.index('animal');
-    return animalIndex.getAll('cat'); // here we query the objects from the store that have a cat
-  }).then(function(people) {
-    console.log('Cat people:', people);
-  });
-
-  dbPromise.then(function(db) {
-    var tx = db.transaction('people');
-    var peopleStore = tx.objectStore('people');
-    var ageIndex = peopleStore.index('age');
-    return ageIndex.openCursor(); // here we open a cursor
-  }).then(function(cursor) {
-    if (!cursor) return;
-    return cursor.advance(2); // if we want to skip entries
-  }).then(function logPerson(cursor) {
-    if (!cursor) return;
-    console.log('Cursored at:', cursor.value.name);
-    // I could also do things like:
-    // cursor.update(newValue) to change the value, or
-    // cursor.delete() to delete this entry
-    return cursor.continue().then(logPerson); // recursive iteration
-  }).then(function() {
-    console.log('Done cursoring');
-  }); 
-*/
